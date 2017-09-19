6b71d10f289be8922f4f7108a0aeac234dbb6be8
Sam Zhang <samanthazhang9@gmail.com>
2017-09-19 11:32:02 -0400

    added locals

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var express = require( 'express' );
+var nunjucks = require( 'nunjucks' );
 var app = express(); 
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.set('view engine', 'html');
+
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', {noCache: true});
+
+nunjucks.render('index.html', locals, function(err, output) {
+    console.log(output);
+})
 
 app.use('/special/', function (req, res, next) {
     console.log('you reached the special area', req.method);
@@ -12,7 +32,7 @@ app.use(function (req, res, next) {
 })
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index', locals);
 })
 
 app.listen(3000, function() {
